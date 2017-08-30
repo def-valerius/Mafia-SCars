be34ffa5d2240914861e77e57aeeaf7605c37cab
def-valerius <31399259+def-valerius@users.noreply.github.com>
2017-08-30 14:56:19 +0500

    some engine sounds

--- lua/autorun/mafia_horn_sounds.lua
+++ lua/autorun/mafia_horn_sounds.lua
@@ -1,2 +1,3 @@
 list.Set( "SCarHornSounds", "#Mafia Bolt Ace", { carsound_hornsound = "mafia/horn1.wav" } )
 list.Set( "SCarHornSounds", "#Mafia Bolt Model B", { carsound_hornsound = "mafia/horn3.wav" } )
+list.Set( "SCarHornSounds", "#Mafia Bolt V8", { carsound_hornsound = "mafia/horn4.wav" } )
--- lua/autorun/mafia_scar_sounds.lua
+++ lua/autorun/mafia_scar_sounds.lua
@@ -2,3 +2,6 @@ list.Set( "SCarEngineSounds", "#Mafia Bolt Ace 1", { carsound_enginesound = "maf
 list.Set( "SCarEngineSounds", "#Mafia Bolt Ace 2", { carsound_enginesound = "mafia/fordT1_NPC.wav" } )
 list.Set( "SCarEngineSounds", "#Mafia Bolt Model B 1", { carsound_enginesound = "mafia/fordA_NPC.wav" } )
 list.Set( "SCarEngineSounds", "#Mafia Bolt Model B 2", { carsound_enginesound = "mafia/fordA1_NPC.wav" } )
+list.Set( "SCarEngineSounds", "#Mafia Bolt V8", { carsound_enginesound = "mafia/fordV8_NPC.wav" } )
+list.Set( "SCarEngineSounds", "#Mafia Bolt V8 2", { carsound_enginesound = "mafia/fordV8_0.wav" } )
+list.Set( "SCarEngineSounds", "#Mafia Bolt V8 3", { carsound_enginesound = "mafia/fordV8_2.wav" } )
